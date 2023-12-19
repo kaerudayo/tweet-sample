5b1e4bf1d4cc05489f2b783fd91e15807c7344a6
TenSuke0513 <daendo0513@gmail.com>
2023-12-19 14:49:59 +0900

    Twitter demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,84 @@
-import React from "react";
-import { Modal, Button, Text, Input, Row, Checkbox } from "@nextui-org/react";
-import { Mail } from "../components/Mail";
-import { Password } from "../components/Password";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from '../styles/Home.module.css';
 
-export default function App() {
-  const [visible, setVisible] = React.useState(false);
-  const handler = () => setVisible(true);
+const Home = () => {
+  const [tweets, setTweets] = useState([]);
+  const [newTweet, setNewTweet] = useState('');
 
-  const closeHandler = () => {
-    setVisible(false);
-    console.log("closed");
+  useEffect(() => {
+    // json-sever
+    const apiUrl = 'http://localhost:8000/tweets';
+
+    // つぶやきデータを取得する関数
+    const fetchTweets = async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        // id降順に並び替える(投稿が新しい順に並び替える)
+        const sortedTweets = response.data.sort((a, b) => b.id - a.id);
+        setTweets(sortedTweets);
+      } catch (error) {
+        console.error('Error fetching tweets:', error);
+      }
+    };
+    
+    // ロード時にツイートデータを取得
+    fetchTweets();
+  }, []);
+
+  const handleNewTweetChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setNewTweet(event.target.value);
+  };
+
+  const handleAddTweet = async () => {
+    try {
+      // json-sever
+      const apiUrl = 'http://localhost:8000/tweets';
+
+      // 新しいツイートを作成
+      const response = await axios.post(apiUrl, {
+        body: newTweet,
+        //idは空欄にして順次追加
+      });
+
+      // 新しいツイートを最後尾に追加
+      setTweets([...tweets, response.data]);
+
+      // 入力欄をクリア
+      setNewTweet('');
+    } catch (error) {
+      console.error('Error adding tweet:', error);
+    }
   };
 
   return (
-    <div>
-      <Button auto shadow onPress={handler}>
-        Open modal
-      </Button>
-      <Modal
-        closeButton
-        aria-labelledby="modal-title"
-        open={visible}
-        onClose={closeHandler}
-      >
-        <Modal.Header>
-          <Text id="modal-title" size={18}>
-            Welcome to
-            <Text b size={18}>
-              NextUI
-            </Text>
-          </Text>
-        </Modal.Header>
-        <Modal.Body>
-          <Input
-            clearable
-            bordered
-            fullWidth
-            color="primary"
-            size="lg"
-            placeholder="Email"
-            contentLeft={<Mail fill="currentColor" />}
-          />
-          <Input
-            clearable
-            bordered
-            fullWidth
-            color="primary"
-            size="lg"
-            placeholder="Password"
-            contentLeft={<Password fill="currentColor" />}
-          />
-          <Row justify="space-between">
-            <Checkbox>
-              <Text size={14}>Remember me</Text>
-            </Checkbox>
-            <Text size={14}>Forgot password?</Text>
-          </Row>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button auto flat color="error" onPress={closeHandler}>
-            Close
-          </Button>
-          <Button auto onPress={closeHandler}>
-            Sign in
-          </Button>
-        </Modal.Footer>
-      </Modal>
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <p>ホーム</p>
+          <div>
+            <textarea
+              rows="4"
+             cols="50"
+             placeholder="いまなにしてる？"
+             value={newTweet}
+             onChange={handleNewTweetChange}
+            />
+            <br />
+            <button onClick={handleAddTweet}>ツイートする</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <ul>
+          {tweets.map((tweet) => (
+
+            <li key={tweet.id}>{tweet.id}:{tweet.body}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
